c57dd05f645498c756d20a850572e282f2ee067c
Leonardo Arero <65541707+arerobarbudovisk20@users.noreply.github.com>
2023-04-21 17:01:12 -0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,36 +1,49 @@
-class Conta(var titular:String,var saldo:Double,var numero:Int){
-       
-       fun deposita(valor:Double){
-           this.saldo += valor
-       
-       
-       }
+class Conta(var titular: String, var saldo: Double, var numero: Int) {
+    //MÉTODO PARA DEPOSITAR VALOR NA CONTA
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
 
-       fun sacar(valor:Double){
-            if(saldo >= valor){
-                this.saldo -= valor
-            }   
-            if(saldo < valor){
-                println("NÃO FOI POSSÍVEL REALIZAR O SAQUE")
-                println("SALDO INSUCIENTE")
-            }
-           
-       }
-       
+    //MÉTODO PARA SACAR VALOR DA CONTA
+    fun sacar(valor: Double) {
+
+        if (saldo >= valor) {
+            this.saldo -= valor
+        }
+        if (saldo < valor) {
+            println("NÃO FOI POSSÍVEL REALIZAR SAQUE")
+            println("SALDO INSUFICIENTE!")
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.saldo += valor
+            return true
+        } else {
+            println("SALDO INSUFICIENTE")
+            return false
+        }
+    }
+
+
 }
 
-fun main(){
+fun main() {
     println(" ---------- Bem Vindo ao ByteBank ----------")
     println()
     //instanciando os objetos: contaLeonardo, contaMaria
-    val contaLeonardo = Conta("Leonardo",1200.55,1000)
-    val contaMaria = Conta("Maria",1200.22,1001)
+    val contaLeonardo = Conta("Leonardo", 1200.55, 1000)
+    val contaMaria = Conta("Maria", 1200.22, 1001)
 
     println("Seja Bem Vindo(a) ${contaLeonardo.titular}")
     println("Titular: ${contaLeonardo.titular}")
     println("Número: ${contaLeonardo.numero}")
     println("Saldo: ${contaLeonardo.saldo}")
-
+    println("DEPOSITO SENDO REALIZADO NA CONTA: ${contaLeonardo.titular}")
+    contaLeonardo.deposita(234.50)
+    println("SALDO ATUAL: ${contaLeonardo.saldo}")
     println()
     println()
 
@@ -38,19 +51,21 @@ fun main(){
     println("Titular:${contaMaria.titular}")
     println("Número:${contaMaria.numero}")
     println("Saldo:${contaMaria.saldo}")
+    println("DEPOSITO SENDO REALIZADO NA CONTA: ${contaMaria.titular}")
+    contaMaria.deposita(234.50)
+    println("SALDO ATUAL: ${contaMaria.saldo}")
     println()
-    
-    println("SALDO SENDO REALIZADO DA CONTA: $contaMaria.titular")
-    contaMaria.sacar(250.50)
-    println("SALDO ATUAL: $contaMaria.saldo")
 
+    println("SAQUE SENDO REALIZADO NA CONTA: ${contaMaria.titular}")
+    contaMaria.sacar(3.50)
+    println("SALDO ATUAL: ${contaMaria.saldo}")
 
-
-
-
-
+    println("TRANSFERENCIA DA CONTA: ${contaLeonardo.titular} PARA A CONTA: ${contaMaria.titular}")
+    contaLeonardo.transfere(20000.0, contaMaria)
+    println("SALDO ATUAL DA CONTA MARIA: ${contaMaria.saldo}")
 
 }
+
 
 /*
 fun testaCopiaEReferencia(){
