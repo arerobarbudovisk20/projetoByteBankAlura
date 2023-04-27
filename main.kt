b146aee7f3a0abd158c61a1532db07faebaf66eb
Leonardo Arero <65541707+arerobarbudovisk20@users.noreply.github.com>
2023-04-27 03:24:08 -0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,5 +1,17 @@
-class Conta(var titular: String, var saldo: Double, var numero: Int) {
+class Conta() {
+
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+        private set(valor) {
+            if (valor > 0){
+                field = valor
+            }
+        }
+
+
     //MÉTODO PARA DEPOSITAR VALOR NA CONTA
+
     fun deposita(valor: Double) {
         this.saldo += valor
     }
@@ -19,7 +31,7 @@ class Conta(var titular: String, var saldo: Double, var numero: Int) {
     fun transfere(valor: Double, destino: Conta): Boolean {
         if (saldo >= valor) {
             saldo -= valor
-            destino.saldo += valor
+            destino.deposita(valor)
             return true
         } else {
             println("SALDO INSUFICIENTE")
@@ -27,15 +39,21 @@ class Conta(var titular: String, var saldo: Double, var numero: Int) {
         }
     }
 
-
 }
 
 fun main() {
     println(" ---------- Bem Vindo ao ByteBank ----------")
     println()
     //instanciando os objetos: contaLeonardo, contaMaria
-    val contaLeonardo = Conta("Leonardo", 1200.55, 1000)
-    val contaMaria = Conta("Maria", 1200.22, 1001)
+    val contaLeonardo = Conta()
+    contaLeonardo.titular = "Leonardo"
+    contaLeonardo.numero = 1000
+
+
+    val contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaMaria.numero = 1001
+
 
     println("Seja Bem Vindo(a) ${contaLeonardo.titular}")
     println("Titular: ${contaLeonardo.titular}")
@@ -61,25 +79,7 @@ fun main() {
     println("SALDO ATUAL: ${contaMaria.saldo}")
 
     println("TRANSFERENCIA DA CONTA: ${contaLeonardo.titular} PARA A CONTA: ${contaMaria.titular}")
-    contaLeonardo.transfere(20000.0, contaMaria)
+    contaLeonardo.transfere(25.0, contaMaria)
     println("SALDO ATUAL DA CONTA MARIA: ${contaMaria.saldo}")
 
 }
-
-
-/*
-fun testaCopiaEReferencia(){
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
-    println(numeroX)
-    println(numeroY)
-
-    val contaJoao = Conta("Joao",0.0,0)
-    var contaJulia = contaJoao
-    contaJulia.titular = "Julia"
-    println(contaJoao.titular)
-    println(contaJulia.titular)
-
-}
-*/
