c7cbc8b1aec2b530ac6d6837ae7e21612c50f15f
Leonardo Arero <65541707+arerobarbudovisk20@users.noreply.github.com>
2023-05-12 23:37:03 -0300

    Create testaComportamentoConta.kt

---
+++ testaComportamentoConta.kt
@@ -0,0 +1,40 @@
+fun testaComportamentoConta() {
+    val contaLeonardo = Conta(titular = "LEONARDO", numero = 1000)
+    val contaMaria = Conta(titular = "MARIA", numero = 1001)
+    val contaJulio = Conta(titular = "JULIO", numero = 1002)
+
+
+    println("Seja Bem Vindo(a) ${contaLeonardo.titular}")
+    println("Titular: ${contaLeonardo.titular}")
+    println("Número: ${contaLeonardo.numero}")
+    println("Saldo: ${contaLeonardo.saldo}")
+    println("DEPOSITO SENDO REALIZADO NA CONTA: ${contaLeonardo.titular}")
+    contaLeonardo.deposita(234.50)
+    println("SALDO ATUAL: ${contaLeonardo.saldo}")
+    println()
+    println()
+
+    println("Seja Bem Vindo(a) ${contaMaria.titular}")
+    println("Titular:${contaMaria.titular}")
+    println("Número:${contaMaria.numero}")
+    println("Saldo:${contaMaria.saldo}")
+    println("DEPOSITO SENDO REALIZADO NA CONTA: ${contaMaria.titular}")
+    contaMaria.deposita(234.50)
+    println("SALDO ATUAL: ${contaMaria.saldo}")
+    println()
+
+    println("Seja Bem Vindo(a) ${contaJulio.titular}")
+    println("Titular: ${contaJulio.titular}")
+    println("Número: ${contaJulio.numero}")
+    println("Saldo: ${contaJulio.saldo}")
+    println()
+    println("SAQUE SENDO REALIZADO NA CONTA: ${contaMaria.titular}")
+    contaMaria.sacar(3.50)
+    println("SALDO ATUAL: ${contaMaria.saldo}")
+
+    println("TRANSFERENCIA DA CONTA: ${contaLeonardo.titular} PARA A CONTA: ${contaMaria.titular}")
+    contaLeonardo.transfere(valor = 25.0, destino = contaMaria)
+    println("SALDO ATUAL DA CONTA MARIA: ${contaMaria.saldo}")
+    testaCopiasEReferencias()
+    testaLacos()
+}
